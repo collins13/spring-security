e522dd30ade026dedbdba54be0cbc074b721055b
rashi collins <rashicollins@dlp.srv.world>
2023-10-25 17:48:29 +0300

    changes

--- src/main/java/com/codingachine/security/config/JwtService.java
+++ src/main/java/com/codingachine/security/config/JwtService.java
@@ -2,11 +2,18 @@ package com.codingachine.security.config;
 
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
 import java.security.Key;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Function;
 
 @Service
@@ -16,10 +23,39 @@ public class JwtService {
         return extractClaims(token, Claims::getSubject);
     }
 
+
     public <T> T extractClaims(String token, Function<Claims, T> claimResolver){
         final  Claims claims = extractAllClaims(token);
         return claimResolver.apply(claims);
     }
+
+    public String generateToken(UserDetails userDetails){
+        return generateToken(new HashMap<>(), userDetails);
+    }
+
+    public boolean isTokenValid(String token, UserDetails userDetails){
+        final String username = extractUsername(token);
+        return (username.equals(userDetails.getUsername())) && !isTokeExpired(token);
+    }
+
+    private boolean isTokeExpired(String token) {
+        return extractExpiration(token).before(new Date());
+    }
+
+    private Date extractExpiration(String token) {
+        return extractClaims(token, Claims::getExpiration);
+    }
+
+    public String generateToken(Map<String, Object> extraClaims, UserDetails userDetails){
+        return Jwts.builder()
+                .setClaims(extraClaims)
+                .setSubject(userDetails.getUsername())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60 * 24))
+                .signWith(getSignInKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
 
     private Claims extractAllClaims(String token){
         return Jwts
