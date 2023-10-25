da27c8a34bc2b0d587ebc8f979f1f9b3135350ea
rashi collins <rashicollins@dlp.srv.world>
2023-10-25 09:28:13 +0300

    added some new jwt configuration

---
+++ src/main/java/com/codingachine/security/config/JwtService.java
@@ -0,0 +1,37 @@
+package com.codingachine.security.config;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.function.Function;
+
+@Service
+public class JwtService {
+    private final static String SECRET_KEY = "wSmVk5N0lcFsFCWDDZcvwSs4SC3JtFyN";
+    public String extractUsername(String token) {
+        return extractClaims(token, Claims::getSubject);
+    }
+
+    public <T> T extractClaims(String token, Function<Claims, T> claimResolver){
+        final  Claims claims = extractAllClaims(token);
+        return claimResolver.apply(claims);
+    }
+
+    private Claims extractAllClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody();
+    }
+
+    private Key getSignInKey() {
+        byte[] keyBites = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBites);
+    }
+}
